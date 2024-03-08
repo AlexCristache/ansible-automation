e3053dc7beb7535c2a0f42423059a3dacfd2b962
Alex Cristache <mc14468@my.bristo.ac.uk>
2024-03-08 20:34:54 +0200

    restructure the project and add nvim and some core utils

--- bin/start.sh
+++ bin/start.sh
@@ -4,7 +4,7 @@ set -e
 
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-ansible-playbook --diff\
+ansible-playbook --diff --check\
   -i "${PROJECT_ROOT}/hosts" "${PROJECT_ROOT}/main.yaml" \
   --ask-become-pass --ask-vault-pass \
   -e "ansible_python_interpreter=$(which python3)"
